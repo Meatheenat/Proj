9c2211351c0c0ba95d5bd609572ff102484f387d
Meatheenat <Meatheenat.k@gmail.com>
2026-02-19 15:30:44 +0700

    40

--- login.php
+++ login.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>เข้าสู่ระบบ - Library System</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+   
     <link rel="stylesheet" href="assets/style.css">
     <style>
         body { height: 100vh; display: flex; flex-direction: column; align-items: center; justify-content: center; }
